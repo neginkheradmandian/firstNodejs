3b3f29340a80597b8e7f8b16d661ec8ae746f7f0
neginkheradmandian@gmail.com <neginkheradmandian@gmail.com>
2022-02-20 09:48:16 +0100

    test url and event

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const person=require('./person');
+// const person=require('./person');
 
-const person1 = new person('Negin' , 34);
-person1.greeting();
+// const person1 = new person('Negin' , 34);
+// person1.greeting();
+
+const Logger = require('./reference/logger');
+
+const logger = new Logger();
+logger.on('message' , (data)=> console.log('Called Listener:' ,data));
+
+logger.log('Hello world');
---
+++ reference/event_demo.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter= new MyEmitter();
+
+myEmitter.on('event', ()=>{
+    console.log('Event fired!');
+})
+
+myEmitter.emit('event');
